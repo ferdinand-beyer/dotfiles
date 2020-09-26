887bbe533e7eb8da8a89038858a62aabbeac32c6
Ferdinand Beyer <fbeyer@thoughtworks.com>
2020-09-26 13:40:49 +0200

    Add zsh-syntax-highlighting

--- zsh/install.sh
+++ zsh/install.sh
@@ -2,13 +2,18 @@
 
 ZSH=$HOME/.zsh
 ZSH_CUSTOM=$ZSH/custom
-P10K=$ZSH_CUSTOM/themes/powerlevel10k
 
 if [[ ! -d "$ZSH" ]]; then
     export ZSH
     sh zsh/oh-my-zsh/install.sh --unattended --keep-zshrc
 fi
 
-if [[ ! -d "$P10K" ]]; then
-    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $P10K
+if [[ ! -d "$ZSH_CUSTOM/themes/powerlevel10k" ]]; then
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git \
+        $ZSH_CUSTOM/themes/powerlevel10k
+fi
+
+if [[ ! -d "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting" ]]; then
+    git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git \
+        $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 fi
