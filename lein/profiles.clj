5b2b94a8ecc11f5e435bf6b56d5911aeb9df2548
Ferdinand Beyer <ferdinand@wefarm.com>
2022-05-29 10:33:36 +0200

    Update lein profiles

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,2 +1,4 @@
-{:user {:dependencies [[org.clojure/tools.namespace "1.2.0"]
-                       [djblue/portal "0.20.1"]]}}
+{:user {:plugins      [[lein-pprint "1.3.2"]]
+        :dependencies [[org.clojure/tools.namespace "1.2.0"]
+                       [djblue/portal "0.20.1"]]
+        :source-paths [#=(eval (str (System/getProperty "user.home") "/.local/dev/clojure/src"))]}}
