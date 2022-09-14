0668604cc171d945dbbc4ead5b81c2f6e2301fff
Ferdinand Beyer <fb@fbeyer.com>
2022-09-14 13:19:16 +0200

    Add git.zsh

---
+++ zsh/custom/git.zsh
@@ -0,0 +1,21 @@
+git_feature_branches() {
+    git branch --no-color |\
+        grep -vE "^([+*]|\s*($(git_main_branch)|$(git_develop_branch))\s*$)"
+}
+
+git_rebase_branches() {
+    local branch
+    local orig="$(git branch --show-current)"
+    for branch in $(git_feature_branches); do
+        echo -e "\033[1;33mBranch: $branch\033[0m"
+        if  git checkout $branch && git rebase main; then
+            echo -e "\033[1;32mOK.\033[0m"
+        else
+            git rebase --abort
+            echo -e "\033[1;31mAborted.\033[0m"
+        fi
+    done
+    git checkout $orig
+}
+
+alias gsync='gcm && gfa && git rebase && git_rebase_branches && gbda'
