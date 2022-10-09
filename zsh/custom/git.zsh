55b17c17d82b84460aed01156692123472933698
Ferdinand Beyer <fb@fbeyer.com>
2022-10-09 14:46:28 +0200

    Support different git main branches

--- zsh/custom/git.zsh
+++ zsh/custom/git.zsh
@@ -6,9 +6,10 @@ git_feature_branches() {
 git_rebase_branches() {
     local branch
     local orig="$(git branch --show-current)"
+    local main="$(git_main_branch)"
     for branch in $(git_feature_branches); do
         echo -e "\033[1;33mBranch: $branch\033[0m"
-        if  git checkout $branch && git rebase main; then
+        if  git checkout $branch && git rebase $main; then
             echo -e "\033[1;32mOK.\033[0m"
         else
             git rebase --abort
